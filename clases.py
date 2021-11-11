c64ad7de065e7ae23c147d942db300f15b894a02
manuelsebastian12 <manuelsebastiansalvador7@gmail.com>
2021-11-10 23:36:48 -0600

    Creación de algunas clases

--- clases.py
+++ clases.py
@@ -1,5 +1,15 @@
 """En este archivo se realizan las clases"""
 
 class Nombre:
-    def __init(self)
+    def __init(self, nombre):
+        self.nombre = nombre
+
+class Gerente(Nombre):
+    def __init(self):
         pass
+
+    def entrevistar(self):
+        print("es el gerente de la empresa")
+
+class empleado():
+    pass
