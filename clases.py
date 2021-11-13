86eb2ae185c7725aee68434ffbdac780e59c84d6
manuelsebastian12 <manuelsebastiansalvador7@gmail.com>
2021-11-13 00:41:23 -0600

    Replanteamiento

--- clases.py
+++ clases.py
@@ -3,19 +3,3 @@
 class Empresa:
     def __init__(self):
         pass
-
-    def trabajar(self):
-        Print("El empleado esta realizando")
-class Nombre:
-    def __init(self, nombre):
-        self.nombre = nombre
-
-class Gerente(Nombre):
-    def __init(self):
-        pass
-
-    def entrevistar(self):
-        print("es el gerente de la empresa")
-
-class empleado():
-    pass
