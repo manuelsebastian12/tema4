95ee79766034bd2347463778958b99e19f8ce5dc
manuelsebastian12 <manuelsebastiansalvador7@gmail.com>
2021-11-13 21:00:25 -0600

    Creación de algunas clases

--- clases.py
+++ clases.py
@@ -1,5 +1,41 @@
 """En este archivo se realizan las clases"""
 
-class Empresa:
+class Persona():
+    def __init__(self, nombre):
+        self.nombre = nombre
+        self.__estatura = "1.85 m"
+
+    def trabajar(self):
+        pass
+
+    def getEstatura(self):
+        return print("Su estatura es de ", self.__estatura)
+
+class Empresa():
     def __init__(self):
         pass
+    def NombreEmpresa(self):
+        print("En la empresa multinacional M&MTechnologies")
+
+class Gerente(Persona, Empresa):
+    def __init__(self, nombre):
+        super().__init__(nombre)
+        pass
+
+    def entrevistar(self):
+        b = self.nombre
+        print("El gerente se llama ", b, ", en este momento está entrevistando a un candidato para una vacante")
+
+    def trabajar(self):
+        print("Más tarde el gerente realizará una supercisión a las diferentes áreas")
+
+    def vestir(self):
+        print("El gerente viste un traje de color azul y una corbata roja")
+
+class Empleado(Persona, Empresa):
+    def __init__(self, nombre):
+        super().__init__(nombre)
+        pass
+
+    def trabajar(self):
+        print("El empleado está realizando sus actividades correspondientes")
